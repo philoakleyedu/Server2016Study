029200939323be554fc6be1e910a1cb874ffd4c3
philoakley <phil.oakley@education.gov.uk>
2017-11-26 01:37:04 +0000

    s

--- Study/Azure/Get-AzureKeyVaultResources.ps1
+++ Study/Azure/Get-AzureKeyVaultResources.ps1
@@ -5,16 +5,15 @@
 
 Login-AzureRmAccount
 $Subs = Get-AzureRmSubscription
-$AllVaults = "Subscription Name : Resource Group : KeyVault "
 ForEach ($Sub in $Subs) {
     Set-AzureRmContext -SubscriptionName $Sub.Name
     $ResourceGroups = Get-AzureRmResourceGroup   
     foreach ($ResourceGroup in $ResourceGroups) {
         $keyvaults = Get-AzureRmResource -ResourceGroupName $ResourceGroup.ResourceGroupName -ResourceType Microsoft.KeyVault/vaults
         if ($keyvaults -ne $null ) {
-            $AllVaults += $Sub.Name + $ResourceGroup.ResourceGroupName + $keyvaults.Name
+            $AllVaults += $keyvaults.Name
         }
     }
 }
-
-Write-Host $AllVaults
+foreach ($Vault in $AllVaults) {
+Write-Host $Vault.Name }
